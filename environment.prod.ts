804cebdf821bc3ae3bb9525fe7dc6fc2e53a1bec
ChitraChauhan <chitrasureshchauhan@gmail.com>
2025-11-03 13:07:30 +0530

    - Added missing add to cart and out of stock label in product card. - Vercel deployment changes.

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  assetsBaseUrl: 'http://localhost:5000/uploads/',
-  apiUrl: 'http://localhost:5000',
+  assetsBaseUrl: 'https://mean-kart-be.vercel.app/uploads/',
+  apiUrl: 'https://mean-kart-be.vercel.app',
   RAZORPAY_KEY_ID: 'rzp_test_RQamGlPahxX3Dd',
   RAZORPAY_KEY_SECRET: 'YF0ufU7yInv6NtbrDbjppfNT',
 };
